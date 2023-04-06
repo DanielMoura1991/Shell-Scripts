145e48f351008e5973585a9ef18f1cfb45a05d94
Daniel Alves <92269045+w3bScr4pp3r@users.noreply.github.com>
2023-04-06 10:08:41 -0300

    Update extra_tools_ubuntu.sh

--- extra_tools_ubuntu.sh
+++ extra_tools_ubuntu.sh
@@ -4,6 +4,3 @@ sudo apt upgrade -y
 sudo apt install ubuntu-restricted-extras -y
 sudo apt install net-tools -y
 sudo apt install nmap -y
-wget https://raw.githubusercontent.com/LionSec/katoolin/master/katoolin.py
-sudo mv katoolin.py /usr/bin/katoolin
-sudo chmod +x /usr/bin/katoolin
